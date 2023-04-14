96a8e02c7b32f0b30fa653e3a46d3b65b7e8c093
tingfeng-key <1770963469@qq.com>
2023-04-14 11:45:39 +0800

    优化

--- app/command/Collect.php
+++ app/command/Collect.php
@@ -56,7 +56,7 @@ class Collect extends Command
                 $output->writeln("start level page:".$value['two_level_name']);
 
                 try {
-                    //                $ql = QueryList::get('https://wordsofwonders.net/en/yellowstone-national-park/level-1003');
+                    //                $ql = QueryList::get('h ttps://wordsofwonders.net/en/yellowstone-national-park/level-1003');
                     $ql = QueryList::get('https://wordsofwonders.net'.$value['link']);
 
                     $value['content'] = $ql->find(".crossword .crossword-row")->map(function ($item) {
@@ -76,6 +76,7 @@ class Collect extends Command
 
                     $value['content2'] = $content2Array;
                 }catch (\Throwable $throwable) {
+                    $output->writeln($throwable->getMessage());
                     continue;
                 }
 
@@ -85,6 +86,18 @@ class Collect extends Command
         file_put_contents("levels.json", json_encode($data));
         $output->writeln('finish');
         return self::SUCCESS;
+    }
+
+    private static function getLevels()
+    {
+        $data = QueryList::get('https://wordsofwonders.net/en/')
+            ->rules([
+                'one_level_name'=>array('','text'),
+                'link'=>array('','href')
+            ])->range(".levels a")
+            ->query()
+            ->getData()
+            ->all();
     }
 
 }
